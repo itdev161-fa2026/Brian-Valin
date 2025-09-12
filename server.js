67526238f4febe755130a35b8e7f93a162807946
Valin <212599415@gehealthcare.com>
2025-09-12 08:53:19 -0500

    Added Validation

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
 import express from 'express';
 import connectDatabase from './config/db.js';
-
+import { check, validationResult } from 'express-validator'
 
 const app = express();
 
 
 connectDatabase();
 
+app.use(express.json({ extended: false }));
 
+/**
+ * @route GET /
+ * @desc Test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+/**
+ * @route Post api/users
+ * @desc Register User
+ */
+app.post('/api/users', [
+    check('name', 'Please enter your name').not().isEmpty(),
+    check('email', 'Please enter a valid email').isEmail(),
+    check('passord', 'Please enter a password with 6 or more characters').isLength({min:6})
+    ], (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            return res.send(req.body);
+        }
+    }
+);
 
 app.listen(3000, () => console.log('express server running on port 3000'));
 
